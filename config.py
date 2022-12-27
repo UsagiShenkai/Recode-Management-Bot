dfaaa94f8fc3f0b80d7fde3d7087d4fa9b0a1949
UsagiShenkai <91740250+UsagiShenkai@users.noreply.github.com>
2022-12-27 12:16:54 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,19 +8,19 @@ if os.path.exists("local.env"):
 load_dotenv()
 class Config(object):
     log = True
-    APP_ID = getenv("API_ID", "6435225")
-    API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    TOKEN = getenv("TOKEN", "")
-    OWNER_ID = getenv("OWNER_ID", "")
-    ASSISTANT_ID = getenv("ASSISTANT_ID", "")
-    STRING_SESSION = getenv("STRING_SESSION", "") #telethon
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "")
-    DB_URI = getenv("DATABASE_URL", "")
+    APP_ID = getenv("API_ID", "9735747")
+    API_HASH = getenv("API_HASH", "391c71286a84db5065e2d218d9cdfd38")
+    TOKEN = getenv("TOKEN", "5888843904:AAEYxHtQNjCx8x-nglIorkueYaG-_DNOKe0")
+    OWNER_ID = getenv("OWNER_ID", "5489233583")
+    ASSISTANT_ID = getenv("ASSISTANT_ID", "5951656665")
+    STRING_SESSION = getenv("STRING_SESSION", "1BVtsOIYBu7JcP8VDGIl-F8ShIEUCaqtbl1k8Qb-doEl3r5sjbk3BiIt4ZJq1EFGCZVA28p6Z4r1WmeIxLdl50Dl5_3DabXTH3RJGwBpvo7n1Z0337UeXFUJss_GGfphZdgbr37GZSXkNwpB_U6IeNFOFAxekUDYubNiUFuhgmkdXgSNAh_cbhIi2nVeL2RNXfd1VBBmJmhbJEj2Rcyf6Tf4UtdiyLjr4xKJOKhKyc5GV7XonHTIvkd9I00_olvbcdm7uS53FnuqlZQzojoRdyEQqz6GiDQG0FvTtnPDBklWedhH7R4qZnJoopUbRwEvCJcnIikvFARcDCMBTW-CogDkPYio0CIg=") #telethon
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "Hirakox")
+    DB_URI = getenv("DATABASE_URL", "postgres://qcocgtqm:xwAT1Iw4TmGsJMwSOJIgv7klpF11qVNH@mahmud.db.elephantsql.com/qcocgtqm")
     DB_URI = DB_URI.replace("postgres", "postgresql")
-    MESSAGE_DUMP = getenv("MESSAGE_DUMP", "-1001509525202")
-    GBAN_LOGS = getenv("GBAN_LOGS", "-1001509525202")
-    SYS_ADMIN = getenv("SYS_ADMIN", "1669178360")
-    DEV_USERS = getenv("DEV_USERS", "1669178360")
+    MESSAGE_DUMP = getenv("MESSAGE_DUMP", "-1001742432410")
+    GBAN_LOGS = getenv("GBAN_LOGS", "-1001742432410")
+    SYS_ADMIN = getenv("SYS_ADMIN", "5951656665")
+    DEV_USERS = getenv("DEV_USERS", "5951656665")
     LOAD = getenv("LOAD")
     WEBHOOK = False
     SPB_MODE = True
@@ -48,6 +48,6 @@ class Config(object):
     CF_API_KEY = getenv("CF_API_KEY", "coffehouse.intellivoid.net")
     BOT_API_URL = getenv("BOT_API_URL", "https://api.telegram.org/bot")
     BOT_API_FILE_URL = getenv("BOT_API_FILE_URL", "https://api.telegram.org/file/bot")
-    SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
-    ZAID_USER = list(map(int, getenv("DEV_USERS", "1669178360").split()))
+    SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5951656665").split()))
+    ZAID_USER = list(map(int, getenv("DEV_USERS", "5951656665").split()))
     NO_LOAD = list(map(int, getenv("NO_LOAD", "").split()))
